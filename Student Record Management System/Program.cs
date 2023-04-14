6cca3291f5aba5b9508d9e1eeb56dfb1ed2f51ed
Aung Pyae Sone <ww.aps2002@gmail.com>
2023-04-13 21:42:28 -0400

    Added Search Student by ID

--- Student Record Management System/Program.cs
+++ Student Record Management System/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Student_Record_Management_System
 {
@@ -46,6 +46,7 @@ namespace Student_Record_Management_System
                         Summary();
                         break;
                     case 6:
+                        Console.Clear();
                         Console.WriteLine("Exitiing Program.");
                         condition = false;
                         break;
@@ -58,21 +59,43 @@ namespace Student_Record_Management_System
         }
         static void AddNewStudent()
         {
-            Console.Write("Enter student's name: ");
-            string name = Console.ReadLine();
-            Console.Write("Enter student's ID: ");
-            int id = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter student's GPA: ");
-            double gpa = Convert.ToDouble(Console.ReadLine());
-              
-            Student student = new Student
+            bool condition = true;
+            while (condition)
             {
-                Name = name,
-                ID = id,
-                GPA = gpa
-            };
-            students.Add(student);
-            Console.Clear();
+                Console.Write("Enter student's name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter student's ID: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter student's GPA: ");
+                double gpa = Convert.ToDouble(Console.ReadLine());
+
+                Student student = new Student
+                {
+                    Name = name,
+                    ID = id,
+                    GPA = gpa
+                };
+                students.Add(student);
+                Console.Clear();
+                char ans;
+                do
+                {
+                    Console.WriteLine("Do you wish to add more student?(Y/N)");
+                    ans = Convert.ToChar(Console.ReadLine());
+                    if (Char.ToUpper(ans) != 'N' && Char.ToUpper(ans) != 'Y')
+                    {
+                        Console.WriteLine("Invalid Input. Please try again.");
+                        Thread.Sleep(1200);
+                        Console.Clear();
+                    }
+
+                } while (Char.ToUpper(ans) != 'N' && Char.ToUpper(ans) != 'Y');
+                if (Char.ToUpper(ans) == 'N')
+                {
+                    Console.Clear();
+                    condition = false;
+                }
+            }
         }
         static void DisplayStudents()
         {
@@ -89,30 +112,37 @@ namespace Student_Record_Management_System
         static void SearchStudent()
         {
             Console.WriteLine("Search the student by:");
-            Console.WriteLine("1. Name.");
-            Console.WriteLine("2. ID. ");
+            Console.WriteLine("1. ID.");
+            Console.WriteLine("2. Name. ");
             Console.WriteLine("3. Go back.");
             int choice = Convert.ToInt32(Console.ReadLine());
-
-            bool condition = true;
-
-            while (condition)
-            {
+            Console.Clear();
+            
                 switch (choice)
                 {
                     case 1:
+                        Console.WriteLine("Enter student's ID");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        Student studentById = students.Find(student => student.ID == id);
+                        if (studentById != null)
+                        {
+                            Console.WriteLine($"Name: {studentById.Name} || ID: {studentById.ID} || GPA: {studentById.ID}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("There is not student with such ID.");
+                        }
+                        Console.WriteLine("Press any key to continue.");
+                        Console.ReadKey();
+                        Console.Clear();
                         break;
                     case 2:
-                        break;
-                    case 3:
-                        condition = false;
-                        Console.Clear();
                         break;
                     default:
                         Console.WriteLine("Invalid Input");
                         break;
                 }
-            }
+            
 
         }
         static void DeleteStudent()
